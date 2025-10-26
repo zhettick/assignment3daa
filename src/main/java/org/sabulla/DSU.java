4da69ba59fd2c0a064ba627e22765a54a3467541
zhettick <belikeotall@gmail.com>
2025-10-26 18:26:43 +0500

    kruskal

---
+++ src/main/java/org/sabulla/DSU.java
@@ -0,0 +1,35 @@
+package org.sabulla;
+
+public class DSU {
+    private int[] parent;
+    private int[] rank;
+
+    public DSU(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x), rootY = find(y);
+
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+    }
+}
--- src/main/java/org/sabulla/KruskalAlgorithm.java
+++ src/main/java/org/sabulla/KruskalAlgorithm.java
@@ -1,9 +1,44 @@
 package org.sabulla;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class KruskalAlgorithm {
     public void runKruskal(int id, List<String> nodeList, List<Edge> edgeList, Metrics metrics) {
+        metrics.start();
 
+        edgeList.sort(Comparator.comparingInt(Edge::getWeight));
+
+        Map<String, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < nodeList.size(); i++) {
+            indexMap.put(nodeList.get(i), i);
+        }
+
+        DSU dsu = new DSU(nodeList.size());
+
+        for (int i = 0; i < edgeList.size(); i++) {
+            Edge edge = edgeList.get(i);
+            int u = indexMap.get(edge.getFrom());
+            int v = indexMap.get(edge.getTo());
+            int rootU = dsu.find(u);
+            metrics.addOperations();
+            int rootV = dsu.find(v);
+            metrics.addOperations();
+
+            if (rootU == rootV) {
+                continue;
+            } else {
+                dsu.union(u, v);
+                metrics.addEdge(edge);
+                metrics.addOperations();
+            }
+
+            if (metrics.getEdgeMST().size() == nodeList.size() - 1) {
+                break;
+            }
+        }
+        metrics.stop();
     }
 }
