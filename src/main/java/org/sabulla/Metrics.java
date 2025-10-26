83a6a83ea720b4e60cb730bb10cdec42eb119bba
zhettick <belikeotall@gmail.com>
2025-10-26 17:15:18 +0500

    metrics

--- src/main/java/org/sabulla/Metrics.java
+++ src/main/java/org/sabulla/Metrics.java
@@ -1,5 +1,45 @@
 package org.sabulla;
 
-public class Metrics {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Metrics {
+    private List<Edge> edgeMST = new ArrayList<>();
+    private int costMST;
+    private int operations;
+    private long startTime;
+    private long time;
+
+    public void addEdge(Edge edge) {
+        edgeMST.add(edge);
+        costMST += edge.getWeight();
+    }
+
+    public void addOperations() {
+        operations++;
+    }
+
+    public void start() {
+        startTime = System.nanoTime();
+    }
+
+    public void stop() {
+        time = System.nanoTime() - startTime;
+    }
+
+    public List<Edge> getEdgeMST() {
+        return edgeMST;
+    }
+
+    public int getCostMST() {
+        return costMST;
+    }
+
+    public int getOperations() {
+        return operations;
+    }
+
+    public long getTime() {
+        return time / 1000000;
+    }
 }
